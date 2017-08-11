298c1b0c72a049d0b9ebf6830c5b5ab7dc9da219
emb3315 <30628212+emb3315@users.noreply.github.com>
2017-08-11 16:24:56 -0500

    Create battleshipBoard.py
    Practicing Python to distract myself.

---
+++ battleshipBoard.py
@@ -0,0 +1,10 @@
+#This will eventually be a one-person game written in Python. 
+#The rules they specify are different. But I think that it should
+#be able to be modified to be two-player. Anywho, wish me luck.
+
+rows = ['O','O','O','O','O']
+
+for row in rows:
+    print
+    for col in rows:
+        print "O ",
