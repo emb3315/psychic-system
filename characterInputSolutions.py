7181f41bf980e4927723569c4fa965b483404453
emb3315 <30628212+emb3315@users.noreply.github.com>
2017-08-11 23:42:16 -0500

    Update characterInputSolutions.py

--- characterInputSolutions.py
+++ characterInputSolutions.py
@@ -1,4 +1,10 @@
 name = input("What is your name: ")
 age = int(input("How old are you: "))
-year = str((2014 - age)+100)
+year = str((2017 - age)+100)
 print(name + " will be 100 years old in the year " + year)
+
+num = input("Give me a number: ")
+if num%2 == 0:
+  print("You're even Steven")
+else:
+  print("You're up shit creek.")
